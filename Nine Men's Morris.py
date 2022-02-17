2f57a6ee7adfe256dce58f911298a8faef52a9d9
Tom Pollack <36968454+Clutchkarma@users.noreply.github.com>
2022-02-17 10:10:00 -0500

    initial board and space setup
    set up the basics of the object Space for representing the spaces on the board

--- Nine Men's Morris.py
+++ Nine Men's Morris.py
@@ -1,14 +1,14 @@
 P1Pieces = 9 #number of pieces in white player's reserve
 P2Pieces = 9 #number of pieces in black player's reserve
-
 class Space:
-    def __init__(self):
-        self.data = '' #piece currently occupying the space
-        self.surrounding = [["",0,0],["",0,0],["",0,0],["",0,0]] #spaces that can be moved to, starting at top then moving clockwise
-    def __init__(self, surrounding):
-        self.data = '' # piece currently occupying the space
+    def __init__(self, surrounding = [], data = ''):
+        self.data = data # piece currently occupying the space
         self.surrounding = surrounding #spaces that can be moved to, starting at top then moving clockwise
+    def setPiece(self, piece):
+        self.data = piece
 
-b1 = [['','',''],['','',''],['','','']]#innermost board
-b2 = [['','',''],['','',''],['','','']]#middle board
-b3 = [['','',''],['','',''],['','','']]#outer board
+b1 = [[Space(),Space(),Space()],[Space(),Space(),Space()],[Space(),Space(),Space()]] #innermost board
+b2 = [[Space(),Space(),Space()],[Space(),Space(),Space()],[Space(),Space(),Space()]] #middle board
+b3 = [[Space(),Space(),Space()],[Space(),Space(),Space()],[Space(),Space(),Space()]] #outer board
+
+quit()
