69a03e028f443181cc034cc02852d8e2338ea501
Tom Pollack <36968454+Clutchkarma@users.noreply.github.com>
2022-02-17 09:49:18 -0500

    Update Nine Men's Morris.py

--- Nine Men's Morris.py
+++ Nine Men's Morris.py
@@ -1,5 +1,13 @@
-P1Pieces = 9
-P2Pieces = 9
+P1Pieces = 9 #number of pieces in white player's reserve
+P2Pieces = 9 #number of pieces in black player's reserve
+
+class Space:
+    def __init__(self):
+        self.data = '' #piece currently occupying the space
+        self.surrounding = [["",0,0],["",0,0],["",0,0],["",0,0]] #spaces that can be moved to, starting at top then moving clockwise
+    def __init__(self, surrounding):
+        self.data = '' # piece currently occupying the space
+        self.surrounding = surrounding #spaces that can be moved to, starting at top then moving clockwise
 
 b1 = [['','',''],['','',''],['','','']]#innermost board
 b2 = [['','',''],['','',''],['','','']]#middle board
